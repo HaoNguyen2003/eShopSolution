2ad4f917a089dd61568e0603381a0a38eb3ae42a
HaoNguyen2003 <129606755+HaoNguyen2003@users.noreply.github.com>
2024-10-11 23:57:26 +0700

    Add AspNetRoleAccessService In businesslayer

---
+++ eShopSolution.BusinessLayer/Service/AspNetRoleAccessService.cs
@@ -0,0 +1,48 @@
+﻿using eShopSolution.BusinessLayer.Abstract;
+using eShopSolution.DataLayer.Abstract;
+using eShopSolution.DataLayer.EntityFramework;
+using eShopSolution.DtoLayer.Model;
+using eShopSolution.DtoLayer.RepositoryModel;
+using eShopSolution.EntityLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.BusinessLayer.Service
+{
+    public class AspNetRoleAccessService : IAspNetRoleAccessService
+    {
+        private readonly IAspNetRoleAccessDal _aspNetRoleAccessDal;
+
+        public AspNetRoleAccessService(IAspNetRoleAccessDal aspNetRoleAccessDal)
+        {
+            _aspNetRoleAccessDal=aspNetRoleAccessDal;
+        }
+        public async Task<BaseRep<string>> Create(RoleAccessModel model)
+        {
+            return await _aspNetRoleAccessDal.Create(model);
+        }
+
+        public async Task<BaseRep<string>> Delete(int ID)
+        {
+            return await _aspNetRoleAccessDal.Delete(ID);
+        }
+
+        public async Task<BaseRep<List<RoleAccessModel>>> GetAll()
+        {
+            return await _aspNetRoleAccessDal.GetAll();
+        }
+
+        public async Task<BaseRep<RoleAccessModel>> GetByID(int ID)
+        {
+            return await _aspNetRoleAccessDal.GetByID(ID);
+        }
+
+        public async Task<BaseRep<string>> Update(int ID, RoleAccessModel model)
+        {
+            return await _aspNetRoleAccessDal.Update(ID, model);
+        }
+    }
+}
