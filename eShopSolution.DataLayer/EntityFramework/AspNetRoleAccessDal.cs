4c2d94b2446c8d9c75cbc40bd800dc4cda3a00c3
HaoNguyen2003 <129606755+HaoNguyen2003@users.noreply.github.com>
2024-10-11 23:58:30 +0700

    Add AspnetRoleAccessDal In Datalayer

---
+++ eShopSolution.DataLayer/EntityFramework/AspNetRoleAccessDal.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using eShopSolution.DataLayer.Abstract;
+using eShopSolution.DataLayer.Context;
+using eShopSolution.DtoLayer.Model;
+using eShopSolution.EntityLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.DataLayer.EntityFramework
+{
+    public class AspNetRoleAccessDal : GenericDal<RoleAccessModel, AspNetRoleAccess>, IAspNetRoleAccessDal
+    {
+        public AspNetRoleAccessDal(ApplicationContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+    }
+}
