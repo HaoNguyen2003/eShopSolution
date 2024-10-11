c247f09579594c82c1f65c55442bd0769faeed79
HaoNguyen2003 <129606755+HaoNguyen2003@users.noreply.github.com>
2024-10-11 23:56:52 +0700

    Add Interface IAspNetRoleAccessService In BusinessLayer

---
+++ eShopSolution.BusinessLayer/Abstract/IAspNetRoleAccessService.cs
@@ -0,0 +1,15 @@
+﻿using eShopSolution.BusinessLayer.Abstract;
+using eShopSolution.DtoLayer.Model;
+using eShopSolution.EntityLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.BusinessLayer.Abstract
+{
+    public interface IAspNetRoleAccessService:IGenericService<RoleAccessModel,AspNetRoleAccess>
+    {
+    }
+}
