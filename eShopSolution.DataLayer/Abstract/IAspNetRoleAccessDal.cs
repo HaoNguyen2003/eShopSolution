718a0585ec44391174c77544660f9cb90e1dec2d
HaoNguyen2003 <129606755+HaoNguyen2003@users.noreply.github.com>
2024-10-11 23:57:55 +0700

    add IAspNetRoleAccessDal In datalayer

---
+++ eShopSolution.DataLayer/Abstract/IAspNetRoleAccessDal.cs
@@ -0,0 +1,14 @@
+﻿using eShopSolution.DtoLayer.Model;
+using eShopSolution.EntityLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.DataLayer.Abstract
+{
+    public interface IAspNetRoleAccessDal:IGenericDal<RoleAccessModel,AspNetRoleAccess>
+    {
+    }
+}
