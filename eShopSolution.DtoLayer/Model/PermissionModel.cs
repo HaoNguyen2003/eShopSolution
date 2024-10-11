b33380283e70508e234dbc50b8182a48f1d36384
HaoNguyen2003 <129606755+HaoNguyen2003@users.noreply.github.com>
2024-10-11 13:11:25 +0700

    add permission model

---
+++ eShopSolution.DtoLayer/Model/PermissionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.DtoLayer.Model
+{
+    public class PermissionModel
+    {
+        public int ID {  get; set; }
+        public string PermissionName { get; set; }
+    }
+}
