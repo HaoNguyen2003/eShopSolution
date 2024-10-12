864b51a30dcdc0ee9fc24b2e72b69949c315dd4b
HaoNguyen2003 <129606755+HaoNguyen2003@users.noreply.github.com>
2024-10-12 16:10:12 +0700

    create interface Role-based access control service

---
+++ eShopSolution.BusinessLayer/Abstract/IRBACService.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.DtoLayer.AddModel;
+using eShopSolution.DtoLayer.Model;
+using eShopSolution.DtoLayer.RepositoryModel;
+using eShopSolution.EntityLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.BusinessLayer.Abstract
+{
+    public interface IRBACService
+    {
+        public Task<Response<List<PolicyModel>>> GetAllPermissionOfUser(string UserID);
+        public Task<Response<List<PolicyModel>>> CreateRoleAndPermission(AddRole role, List<PermissionMenuModel> permissionMenuModels);
+        public Task<Response<string>> CreateRoleAccess(RoleAccessModel roleAccessModel);
+        public Task<Response<string>> DeleteRoleAccess(int ID);
+        public Task<Response<List<PolicyModel>>> GetAllPermissionOfRole(string RoleID);
+
+    }
+}
