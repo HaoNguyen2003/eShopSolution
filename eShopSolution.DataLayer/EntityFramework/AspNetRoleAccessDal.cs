b751d546b5689a90b372eef09e2d11ca233f4fc5
HaoNguyen2003 <129606755+HaoNguyen2003@users.noreply.github.com>
2024-10-12 16:15:04 +0700

    deloyment GetAllRoleAccessModel

--- eShopSolution.DataLayer/EntityFramework/AspNetRoleAccessDal.cs
+++ eShopSolution.DataLayer/EntityFramework/AspNetRoleAccessDal.cs
@@ -3,6 +3,7 @@ using eShopSolution.DataLayer.Abstract;
 using eShopSolution.DataLayer.Context;
 using eShopSolution.DtoLayer.Model;
 using eShopSolution.EntityLayer.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,11 @@ namespace eShopSolution.DataLayer.EntityFramework
     {
         public AspNetRoleAccessDal(ApplicationContext context, IMapper mapper) : base(context, mapper)
         {
+        }
+        public async Task<List<RoleAccessModel>> GetAllRoleAccessModel(string RoleID)
+        {
+            var resultRoleAccessModels = await _context.AspNetRoleAccesses.Where(x=>x.RoleID==RoleID).ToListAsync();
+            return _mapper.Map<List<RoleAccessModel>>(resultRoleAccessModels);
         }
     }
 }
