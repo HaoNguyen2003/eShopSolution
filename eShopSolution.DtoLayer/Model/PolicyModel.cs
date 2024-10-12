ebda645fb9aad015b67876300a92678028c433e7
HaoNguyen2003 <129606755+HaoNguyen2003@users.noreply.github.com>
2024-10-12 16:17:06 +0700

    Create class PolicyModel

---
+++ eShopSolution.DtoLayer/Model/PolicyModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.DtoLayer.Model
+{
+    public class PolicyModel
+    {
+        public int PermissionMenuID { get; set; }
+        public PermissionModel permission { get; set; }
+        public MenuModel menu { get; set; }
+
+    }
+}
