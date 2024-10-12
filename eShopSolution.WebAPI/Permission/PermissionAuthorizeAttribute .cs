94ad34e796266ab743c2dfa432b78f274b497afc
HaoNguyen2003 <129606755+HaoNguyen2003@users.noreply.github.com>
2024-10-12 16:18:27 +0700

    Add Class relate permission

---
+++ eShopSolution.WebAPI/Permission/PermissionAuthorizeAttribute .cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Reflection;
+using System.Security.Claims;
+
+namespace eShopSolution.WebAPI.Permission
+{
+    public class PermissionAuthorizeAttribute : AuthorizeAttribute
+    {
+        public string Permission { get; }
+
+        public PermissionAuthorizeAttribute(string permission)
+        {
+            Permission = permission;
+        }
+    }
+}
