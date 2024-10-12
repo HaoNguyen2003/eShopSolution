f4730f29787be20cc78bb3bad945a8f6c0e9220c
HaoNguyen2003 <129606755+HaoNguyen2003@users.noreply.github.com>
2024-10-12 22:08:27 +0700

    Add Class AddRoleAndPermissions

---
+++ eShopSolution.DtoLayer/Model/AddRoleAndPermissions.cs
@@ -0,0 +1,15 @@
+﻿using eShopSolution.DtoLayer.AddModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.DtoLayer.Model
+{
+    public class AddRoleAndPermissions
+    {
+        public AddRole role {  get; set; }
+        public List<PermissionMenuModel> permissionMenuModels {  get; set; }
+    }
+}
