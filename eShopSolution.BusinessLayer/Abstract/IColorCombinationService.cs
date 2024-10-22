c047b196abaee4ab14d189febf82499a9c46829f
Nguyễn Tiến Hảo <nguyentienhao12.3tpk@gmail.com>
2024-10-22 08:30:19 +0700

    add IColorCombination

---
+++ eShopSolution.BusinessLayer/Abstract/IColorCombinationService.cs
@@ -0,0 +1,16 @@
+﻿using eShopSolution.DtoLayer.Model;
+using eShopSolution.EntityLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.BusinessLayer.Abstract
+{
+    public interface IColorCombinationService : IGenericService<ColorCombinationModel,ColorCombination>
+    {
+        public Task<int> GetColorCombinationIdIfExists(List<int> colorIds);
+        public Task<int> CreateModel(ColorCombinationModel colorCombinationModel);
+    }
+}
