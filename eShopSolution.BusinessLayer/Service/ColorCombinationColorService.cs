59ec67479349537a3c662cd7e1bec62ba1618874
Nguyễn Tiến Hảo <nguyentienhao12.3tpk@gmail.com>
2024-10-22 08:32:39 +0700

    Implement IColorCombinationColorService

---
+++ eShopSolution.BusinessLayer/Service/ColorCombinationColorService.cs
@@ -0,0 +1,46 @@
+﻿using eShopSolution.BusinessLayer.Abstract;
+using eShopSolution.DataLayer.Abstract;
+using eShopSolution.DataLayer.EntityFramework;
+using eShopSolution.DtoLayer.Model;
+using eShopSolution.DtoLayer.RepositoryModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.BusinessLayer.Service
+{
+    public class ColorCombinationColorService : IColorCombinationColorService
+    {
+        private readonly IColorCombinationColorDal _colorCombinationColorDal;
+
+        public ColorCombinationColorService(IColorCombinationColorDal colorCombinationColorDal) {
+            _colorCombinationColorDal=colorCombinationColorDal;
+        }
+        public async Task<BaseRep<string>> Create(ColorCombinationColorModel model)
+        {
+            return await _colorCombinationColorDal.Create(model);
+        }
+
+        public async Task<BaseRep<string>> Delete(int ID)
+        {
+            return await _colorCombinationColorDal.Delete(ID);
+        }
+
+        public async Task<BaseRep<List<ColorCombinationColorModel>>> GetAll()
+        {
+            return await _colorCombinationColorDal.GetAll();
+        }
+
+        public async Task<BaseRep<ColorCombinationColorModel>> GetByID(int ID)
+        {
+            return await _colorCombinationColorDal.GetByID(ID);
+        }
+
+        public async Task<BaseRep<string>> Update(int ID, ColorCombinationColorModel model)
+        {
+            return await _colorCombinationColorDal.Update(ID, model);
+        }
+    }
+}
