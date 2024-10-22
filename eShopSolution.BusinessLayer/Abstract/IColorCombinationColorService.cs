6278c58007608e34ba150259664fc3ce7cbb0db1
Nguyễn Tiến Hảo <nguyentienhao12.3tpk@gmail.com>
2024-10-22 08:29:42 +0700

    Add ColorCombinationservice

---
+++ eShopSolution.BusinessLayer/Abstract/IColorCombinationColorService.cs
@@ -0,0 +1,16 @@
+﻿using eShopSolution.DtoLayer.Model;
+using eShopSolution.EntityLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.BusinessLayer.Abstract
+{
+    public interface IColorCombinationColorService : IGenericService<ColorCombinationColorModel,ColorCombinationColor>
+    {
+
+    }
+}
