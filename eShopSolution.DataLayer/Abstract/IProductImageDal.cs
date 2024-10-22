f321f57397930af335c3faa9b792345d8e9e5d7a
Nguyễn Tiến Hảo <nguyentienhao12.3tpk@gmail.com>
2024-10-22 08:41:25 +0700

    not thing

--- eShopSolution.DataLayer/Abstract/IProductImageDal.cs
+++ eShopSolution.DataLayer/Abstract/IProductImageDal.cs
@@ -8,7 +8,6 @@ namespace eShopSolution.DataLayer.Abstract
         public Task<List<string>> GetAllImageByProductColor(int ID);
         public Task<List<string>> GetAllPublicIDByProductColor(int ID);
         public Task<List<ProductImageModel>> GetAllProductImageByProductColor(int ID);
-
         public Task<string> GetImagefirstByID(int ID);
     }
 }
