1907643a4c5b8ea82f4aceaf059159c3069e7f77
Nguyễn Tiến Hảo <nguyentienhao12.3tpk@gmail.com>
2024-10-22 08:38:20 +0700

    Add logic ColorCombination DB

---
+++ eShopSolution.DataLayer/Abstract/IColorCombinationDal.cs
@@ -0,0 +1,16 @@
+﻿using eShopSolution.DtoLayer.Model;
+using eShopSolution.EntityLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.DataLayer.Abstract
+{
+    public interface IColorCombinationDal : IGenericDal<ColorCombinationModel,ColorCombination>
+    {
+        public Task<int> GetColorCombinationIdIfExists(List<int> colorIds);
+        public Task<int> CreateModel(ColorCombinationModel colorCombinationModel);
+    }
+}
---
+++ eShopSolution.DataLayer/EntityFramework/ColorCombinationDal.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using eShopSolution.DataLayer.Abstract;
+using eShopSolution.DataLayer.Context;
+using eShopSolution.DtoLayer.Model;
+using eShopSolution.EntityLayer.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.DataLayer.EntityFramework
+{
+    public class ColorCombinationDal : GenericDal<ColorCombinationModel, ColorCombination>, IColorCombinationDal
+    {
+        public ColorCombinationDal(ApplicationContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+
+        public async Task<int> CreateModel(ColorCombinationModel colorCombinationModel)
+        {
+            try
+            {
+                var entity = _mapper.Map<ColorCombination>(colorCombinationModel);
+                var result = await _context.ColorCombinations.AddAsync(entity);
+                await _context.SaveChangesAsync();
+                return entity.ID;
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+
+        public async Task<int> GetColorCombinationIdIfExists(List<int> colorIds)
+        {
+            var allColorCombinations = await _context.ColorCombinations.ToListAsync();
+
+            foreach (var colorCombination in allColorCombinations)
+            {
+                var colorCombinationColorIds = _context.ColorCombinationColors
+                    .Where(ccc => ccc.ColorCombinationID == colorCombination.ID)
+                    .Select(ccc => ccc.ColorID)
+                    .ToList();
+
+                if (colorCombinationColorIds.Count == colorIds.Count &&
+                    !colorIds.Except(colorCombinationColorIds).Any() &&
+                    !colorCombinationColorIds.Except(colorIds).Any())
+                {
+                    return colorCombination.ID;
+                }
+            }
+            return -1;
+        }
+    }
+}
