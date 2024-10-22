de8d07c17f6018bb6c178446a4f2f06648f12064
Nguyễn Tiến Hảo <nguyentienhao12.3tpk@gmail.com>
2024-10-22 08:37:41 +0700

    Add Logic Dal for ColorCombinationColor DB

---
+++ eShopSolution.DataLayer/EntityFramework/ColorCombinationColorDal.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using eShopSolution.DataLayer.Abstract;
+using eShopSolution.DataLayer.Context;
+using eShopSolution.DtoLayer.Model;
+using eShopSolution.DtoLayer.RepositoryModel;
+using eShopSolution.EntityLayer.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.DataLayer.EntityFramework
+{
+    public class ColorCombinationColorDal : GenericDal<ColorCombinationColorModel, ColorCombinationColor>, IColorCombinationColorDal
+    {
+        public ColorCombinationColorDal(ApplicationContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+    }
+}
