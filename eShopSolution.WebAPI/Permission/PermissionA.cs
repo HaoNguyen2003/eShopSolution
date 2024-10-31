c63d51fe46009289e4bec02f241b145c7c7e26c2
Nguyễn Tiến Hảo <nguyentienhao12.3tpk@gmail.com>
2024-10-31 22:13:49 +0700

    Create Model PermissionA

---
+++ eShopSolution.WebAPI/Permission/PermissionA.cs
@@ -0,0 +1,41 @@
+﻿using eShopSolution.EntityLayer.Data;
+
+namespace eShopSolution.WebAPI.Permission
+{
+    public static class PermissionA
+    {
+        public const string Address= "Address";
+        public const string Authentication = "Authentication";
+        public const string Brand = "Brand";
+        public const string Category = "Category";
+        public const string CategoryAndBrand = "CategoryAndBrand";
+        public const string Color = "Color";
+        public const string Comments = "Comments";
+        public const string Gender = "Gender";
+        public const string Menu = "Menu";
+        public const string MenuPermission = "MenuPermission";
+        public const string Order = "Order";
+        public const string PaymentMethod = "PaymentMethod";
+        public const string Permissions = "Permissions";
+        public const string ProductColor = "ProductColor";
+        public const string Product = "Product";
+        public const string ProductImage = "ProductImage";
+        public const string ProductReview = "ProductReview";
+        public const string ProductSizeInventory = "ProductSizeInventory";
+        public const string RBAC = "RBAC";
+        public const string Role = "Role";
+        public const string RoleAccess = "RoleAccess";
+        public const string Shipping = "Shipping";
+        public const string Size = "Size";
+        public const string StatusOrder = "StatusOrder";
+        public const string User = "User";
+    }
+    public static class AccessA
+    {
+        public const string Create = "Create";
+        public const string Update = "Update";
+        public const string Get = "Get";
+        public const string Delete = "Delete";
+        public const string Refund = "Refund";
+    }
+}
